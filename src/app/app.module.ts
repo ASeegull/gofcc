016d2de2d0f75dd2f48a523acd154f7d0b758d12
ASeegull <a.chaikalemberg@gmail.com>
2018-01-08 20:16:28 +0200

    Adds navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './header/nav/nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
