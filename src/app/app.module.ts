9ee6eb00faa0acb72ce0f602c3ad4887238d3bd2
ASeegull <a.chaikalemberg@gmail.com>
2018-01-09 14:00:21 +0200

    Adds vertical scrolling to sidebar and editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NavComponent } from './header/nav/nav.component';
 import { SidebarComponent } from './main/sidebar/sidebar.component';
 import { Ch1Component } from './views/basic/ch1/ch1.component';
 import { Ch2Component } from './views/basic/ch2/ch2.component';
+import { EditorComponent } from './main/editor/editor.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { Ch2Component } from './views/basic/ch2/ch2.component';
     NavComponent,
     SidebarComponent,
     Ch1Component,
-    Ch2Component
+    Ch2Component,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
